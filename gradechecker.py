53489592e7d1865317c31369e3e2f2f4fcba8f1b
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-18 13:26:39 +0300

    Update gradechecker.py

--- gradechecker.py
+++ gradechecker.py
@@ -1,35 +1,62 @@
-from selenium import webdriver
 import time
 import winsound
 import datetime
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
 
-# tam yarım saatte bi kickliyo, düzelt
-
-# otomatik login yapması lazım, ama kullanıcı adı ve şifreyi kaydetmesin, ve mac icin de winsound alternatifi
 login_url = "https://kimlik.ege.edu.tr/Identity/Account/Login?ReturnUrl=%2F"
 grade_url = "https://kimlik.ege.edu.tr/Redirect/Redirect?AppEncId=z3Td%2Fth1x8vcvHw%2BDyN0G7GVy9eklCUQxjzDjMFwZaI%3D"
 driver = webdriver.Firefox()
+driver.maximize_window()
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+os.system('cls' if os.name == 'nt' else 'clear') # clears the console for privacy
+
+def login(driver, username, password):
+    driver.get(login_url)
+    time.sleep(2)  
+    username_field = driver.find_element(By.ID, "username")
+    password_field = driver.find_element(By.ID, "password")
+    username_field.send_keys(username)
+    time.sleep(random.randint(1, 3))  
+    password_field.send_keys(password)
+    time.sleep(random.randint(1, 3)) 
+    password_field.send_keys(Keys.RETURN)
+    time.sleep(5) 
 
 def check_for_changes(driver, initial_html):    
     current_html = driver.page_source
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
     if current_html != initial_html:
         print(f"Changes detected at {current_time}!")
-        while True:
-            winsound.Beep(300, 500)  # frequency and duration 
+        if os.name == 'nt':  # Windows
+            while True:
+                winsound.Beep(300, 500)  # frequency and duration 
+        else:  # macOS
+            while True:
+                os.system('say "beep"')
     else:
         print(f"No changes detected at {current_time}.")
 
-driver.get(login_url)
-input("Please log in and press any key.")
+
+login(driver, username, password)
 driver.get(grade_url)
 time.sleep(5)
 initial_html = driver.page_source
-
-# for loop x30 ?? ama random da kullansın, 15-60 saniye arası sürelerle refreshlesin, ama tam 25-30dklık sürede yeni session açsın
-# belki yeni sessionu yan sekmede açıp sekme değişebilir ama zorlar bi tık gereksiz gibi 
+start_time = time.time()
 
 while True:
     check_for_changes(driver, initial_html)
-    driver.get(grade_url)
-    time.sleep(59)
+    driver.get(grade_url)    
+    time.sleep(random.randint(30, 60))
+    
+    if time.time() - start_time > random.randint(1500, 1700):  
+        print("Reloginning!")
+        login(driver, username, password)
+        driver.get(grade_url)
+        time.sleep(5)
+        start_time = time.time()
